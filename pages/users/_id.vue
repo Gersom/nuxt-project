32f998de7870bffda060419c654e3207f1c908fd
gersom <gersomalaja@gmail.com>
2018-05-25 11:41:31 -0500

    [Pages] add users and users/_id

---
+++ pages/users/_id.vue
@@ -0,0 +1,30 @@
+<template lang="html">
+  <div class="">
+    <h2>el {{$route.params.id}}</h2>
+
+    <button @click="back">
+      Regresar
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  },
+
+  methods: {
+    back () {
+      this.$router.push({
+        name: 'users'
+      })
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
---
+++ pages/users/index.vue
@@ -0,0 +1,32 @@
+<template lang="html">
+  <div class="">
+    <h2>Hola?</h2>
+
+    <button v-on:click="toUserId(2)">
+      usuario numero 2
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+    toUserId (id) {
+      this.$router.push({
+        name: 'users-id',
+        params: {
+          id: id
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
