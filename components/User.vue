ae9f18e146398571c1bf39126f248f8e5ddb679a
gersom <gersomalaja@gmail.com>
2018-05-25 11:42:14 -0500

    [components] Add User

---
+++ components/User.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="c-user">
+    <figure>
+      <img src="#" alt="">
+    </figure>
+    <div class="">
+      <p>User name</p>
+      <p>Id</p>
+      <p>Profile</p>
+      <p>Site admin</p>
+    </div>
+  </div>
+</template>
+
+<style>
+
+</style>
