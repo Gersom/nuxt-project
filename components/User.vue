974a49e44c7c446c130413e5c47b2ead68b648a8
gersom <gersomalaja@gmail.com>
2018-05-25 14:01:32 -0500

    [components] add UserList and add props in User

--- components/User.vue
+++ components/User.vue
@@ -1,16 +1,39 @@
-<template>
-  <div class="c-user">
-    <figure>
-      <img src="#" alt="">
-    </figure>
-    <div class="">
-      <p>User name</p>
-      <p>Id</p>
-      <p>Profile</p>
-      <p>Site admin</p>
-    </div>
-  </div>
+<template lang="pug">
+  button.c-user(@click="toUserId(item.id)")
+    figure
+      img(:src="item.avatar_url")
+
+    .c-user__content
+      p User name: {{item.login}}
+      p Id: {{item.id}}
+      p Profile: {{item.html_url}}
+      p Site admin: {{item.site_admin}}
+      p Type: {{item.type}}
 </template>
+
+<script>
+  export default {
+    props: {
+      item: {
+        type: Object,
+        default () {
+          return {}
+        }
+      }
+    },
+
+    methods: {
+      toUserId (id) {
+        this.$router.push({
+          name: 'users-id',
+          params: {
+            id: id
+          }
+        })
+      }
+    }
+  }
+</script>
 
 <style>
 
---
+++ components/UserList.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+  .c-user-list
+    user(v-for="item in list",
+        :item="item",
+        :key="item.id")
+</template>
+
+<script>
+  import User from '~/components/User.vue'
+  export default {
+    name: 'UserList',
+    components: {
+      User
+    },
+    props: {
+      list: {
+        type: Array,
+        default () {
+          return []
+        }
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
